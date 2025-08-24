fee2e3c1c0494bffb5e45e877f25ba6822cc7bbe
Mahidur Rahman <mahidur952697@gmail.com>
2025-08-24 14:36:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// console.log ("connection done");
+// login click js
+document.getElementById("loginButton"). addEventListener("click",function(e){
+    e.preventDefault() 
+    // prevent default er karone click koraro por console.log dekha jaitese
+    // ar ekta chilo btn html e type= button korle eta submit button thake na normal button hoy ar click korle load ne na
+    console.log("CLICK DONE")
+})
+    
